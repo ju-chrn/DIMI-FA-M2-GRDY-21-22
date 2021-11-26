6683bd6fbcdcf1b81c373c9d002117bf42742526
ju-chrn <charon.ju@gmail.com>
2021-11-26 12:44:15 +0100

    final touche

--- guess-the-number/main.js
+++ guess-the-number/main.js
@@ -1,10 +1,17 @@
-const mysteryNumber = 50
+const mysteryNumber = Math.floor(Math.random() * 100) + 1
+let blurRadius = 0
+
+const increaseBlur = () => {
+  blurRadius = blurRadius + 1.5
+  document.body.style.filter = `blur(${blurRadius}px)`
+}
 
 const input = document.querySelector('input')
+const response = document.querySelector('#response')
 
 input.onchange = () => {
   const value = parseInt(input.value)
-  
+
   if (isNaN(value) === true) {
     alert(value)
     alert('Saisie invalide, merci de saisir un nombre.')
@@ -12,9 +19,17 @@ input.onchange = () => {
   }
 
   if (value < mysteryNumber) {
-    document.body.style.backgroundColor = 'pink'
+    response.innerHTML = `Trop petit ! (${value})`
+    increaseBlur()
   }
   if (value > mysteryNumber) {
-    document.body.style.backgroundColor = 'yellow'
+    response.innerHTML = `Trop grand ! (${value})`
+    increaseBlur()
+  }
+  if (value === mysteryNumber) {
+    document.body.style.color = 'white'
+    document.body.style.backgroundColor = '#e8a81b'
+    document.body.style.filter = ''
+    response.innerHTML = 'Bravo !!!'
   }
 }
